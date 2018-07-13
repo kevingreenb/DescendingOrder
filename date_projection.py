5932b1652e2c5bc7df2a943ee96c055942362849
KGreen <kevin.e.green@hotmail.com>
2018-07-12 21:28:19 -0500

    Which Date

---
+++ date_projection.py
@@ -0,0 +1,24 @@
+from datetime import timedelta, datetime
+def which_date(start_date,time):
+    """
+    This function takes as input a string depicting a date in YYYY/mm/dd
+    format and a string stating a time period in the form of "X day(s)" or
+    "Y week(s)". Output should be a string in form YYYY/mm/dd with the date
+    that is X days or Y weeks after the initial date.
+    """
+    
+    # Replace this with your code!
+    temp = time.split();
+    date_object = datetime.strptime(start_date, '%Y/%m/%d')
+    if ((temp[1] == 'days' or temp[1] == 'day')):
+        end_date = date_object+timedelta(days = int(temp[0]));
+    if ((temp[1] == 'weeks' or temp[1] == 'week')):
+        end_date = date_object+timedelta(weeks=int(temp[0]))
+        
+    end_date = f'{end_date:%Y/%m/%d}';
+    return end_date;
+
+print(which_date('2016/02/10','35 days'));
+print(which_date('2016/12/21', '3 weeks'));
+print(which_date('2015/01/17', '1 week'));
+    
